3a71053b440722aa36fe7763c2872336964f8964
mathblender17 <mp3459@srmist.edu.in>
2024-08-11 16:35:05 +0530

    A_sum of digits file updated

--- 964 Div 4/sum of digits.cpp
+++ 964 Div 4/sum of digits.cpp
@@ -1,1 +1,26 @@
+#include <iostream>  
+using namespace std;
 
+int main(){
+    int n,sum;
+    
+    cin>> n ;
+    int arr[n];
+    int ans[n];
+
+    for(int i=0;i<n;i++){
+        sum=0;
+        cin>>arr[i];
+        sum+=arr[i]%10 + (arr[i]/10)%10;
+        ans[i]=sum;
+        
+    }
+    for(int i=0;i<n;i++){
+    cout<<ans[i]<<"\n";
+    }
+    
+
+
+
+    return 0;
+}
